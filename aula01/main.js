719d04bcf4922819e7edb72914d610dbb7384e2e
gabirodri1234567 <gabrielaaraujorodri@gmail.com>
2023-08-24 22:30:45 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,20 @@
+//console.log eu uso para mostrar uma mensagem ao usuario
+console.log("hello");
+// alert mostra um alerta na pagina assim que entramos na página ao usuario
+//alert("isso é um alerta");
+
+
+
+//para declarar variaveis no js podemos usar  "var", "let"ou "const"
+// sempra que usar "const" pra declarar uma variavel , o valor dela não pode ser alterado
+const pi = 3.14;
+alert(pi);
+
+// var e let declaram variavel e ela poderá ser alterada, se comparar ao conts são iguai 
+// mas o let e var são diferentes , o var é uma variavel de escopo  global pode ser usado no codigo interio , o let é de escopo local , só pode usar em um bloco
+// let é uma variavel de escopo local
+// var é uma variavel de escopo global
+
+var nome = "Gabi";
+nome = "maria" // ira aparecer maria pois eu ja declarei o nome na variavel mas eu atualizei com o nome maria 
+alert (nome);
